20cda9416075159f75bdde4705a184f7a046bd50
northbyte53 <nhatquangsin@gmail.com>
2026-05-08 15:28:04 +0700

    fix: update uniffi build ios

--- scripts/mls_uniffi_build_ios.sh
+++ scripts/mls_uniffi_build_ios.sh
@@ -2,7 +2,9 @@
 
 set -eo pipefail
 
-IOS_DEPLOYMENT_TARGET="${IOS_DEPLOYMENT_TARGET:-16.0}"
-export IPHONEOS_DEPLOYMENT_TARGET="$IOS_DEPLOYMENT_TARGET"
+# IOS_DEPLOYMENT_TARGET="${IOS_DEPLOYMENT_TARGET:-16.0}"
+# export IPHONEOS_DEPLOYMENT_TARGET="$IOS_DEPLOYMENT_TARGET"
 
-cargo swift package -y -p "ios@${IOS_DEPLOYMENT_TARGET}" --release -n ios
+IOS_DIR=./ios
+
+cargo swift package -y -p "ios" --release -n ios

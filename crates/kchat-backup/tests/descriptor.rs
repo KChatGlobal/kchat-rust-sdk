0392b8110ca0e77f342891623f42c1277871af8d
northbyte53 <nhatquangsin@gmail.com>
2026-09-18 15:57:34 +0700

    chore: update unit tests

--- crates/kchat-backup/tests/descriptor.rs
+++ crates/kchat-backup/tests/descriptor.rs
@@ -4,6 +4,14 @@ use kchat_backup::{
 };
 
 const MNEMONIC: &str = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon art";
+const FIXTURE_DESCRIPTOR_HEX: &str = "4b43424400015a5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a3f49baa9158794ca47880b7311f0982e1b961a23a195fd43eee37e8c3e08210df99b1f4fc662d301c32b9cd32732cc7884d24a1386bc0e7421d577dd";
+
+fn decode_hex(value: &str) -> Vec<u8> {
+    (0..value.len())
+        .step_by(2)
+        .map(|offset| u8::from_str_radix(&value[offset..offset + 2], 16).unwrap())
+        .collect()
+}
 
 #[test]
 fn seals_a_fixed_size_descriptor_that_opens_to_the_derived_namespace() {
@@ -39,4 +47,17 @@ fn rejects_wrong_length_tampering_and_a_different_authenticated_account() {
     tampered[20] ^= 1;
     assert!(open_descriptor_v1(&master_key, &account, &tampered).is_err());
     assert!(open_descriptor_v1(&master_key, &other_account, &serialized).is_err());
+}
+
+#[test]
+fn opens_the_fixed_v1_descriptor_fixture() {
+    let master_key = BackupMasterKey::from_mnemonic(MNEMONIC).unwrap();
+    let account = BackupAccountId::parse("00112233-4455-6677-8899-aabbccddeeff").unwrap();
+    let serialized = decode_hex(FIXTURE_DESCRIPTOR_HEX);
+
+    assert_eq!(serialized.len(), 90);
+    assert_eq!(
+        open_descriptor_v1(&master_key, &account, &serialized).unwrap(),
+        BackupNamespaceId::derive(&master_key, &account).unwrap()
+    );
 }
